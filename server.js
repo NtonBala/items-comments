6f153adf74e8637803a4b455db724738d4476ca8
NtonBala <balashov.anton.1983@gmail.com>
2017-10-05 20:34:05 +0300

    Added process.env.PORT

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var path = require('path');
 var app = express();
 var favicon = require('serve-favicon');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -10,6 +12,6 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000, (err) => {
+app.listen(app.get('port'), (err) => {
     err ? console.log(err) : console.log('Running on localhost port 9000');
 });
