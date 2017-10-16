bca310d009cad47a7854d391e872d24e625a0b2c
NtonBala <balashov.anton.1983@gmail.com>
2017-10-16 16:54:59 +0300

    Fixed issue with no arbitrary long words & punctuation marks sequence break. Fixed issue with no margin for .content-container in Internet Explorer. Added check for Local Storage support and Message component.

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -2,16 +2,20 @@ import React from 'react';
 import HeaderContainer from '../../containers/HeaderContainer';
 import ItemsContainer from '../../containers/ItemsContainer';
 import CommentsContainer from '../../containers/CommentsContainer';
+import Message from '../widgets/Message';
 
 const MainLayout = () => (
     <div className='main-container'>
         <HeaderContainer/>
-        <main className='content-container'>
-            <div className='clearfix'>
-                <ItemsContainer/>
-                <CommentsContainer/>
-            </div>
-        </main>
+        <div className='content-container'>
+            {typeof(Storage) !== 'undefined' ?
+                <div className='clearfix'>
+                    <ItemsContainer/>
+                    <CommentsContainer/>
+                </div> :
+                <Message/>
+            }
+        </div>
     </div>
 );
 
---
+++ src/components/widgets/Message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Message = () => (
+    <div>
+        <h1>Sorry, no Web Storage support in your browser...</h1>
+    </div>
+);
+
+export default Message;
