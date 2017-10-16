e2e80e31e4a8f478331f092bc054474e15130d89
NtonBala <balashov.anton.1983@gmail.com>
2017-10-16 19:21:00 +0300

    still fixing deployment to heroku

---
+++ src/components/widgets/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = () => (
+    <div className='content-container'>
+        <div className='clearfix'>
+            <h1>Sorry, no Web Storage support in your browser...</h1>
+        </div>
+    </div>
+);
+
+export default Message;
